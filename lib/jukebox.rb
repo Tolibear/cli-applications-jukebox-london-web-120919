249aae9c9766376dd925cbb4c762c2cf040425ee
T Benson <t.benson@live.co.uk>
2019-12-04 16:40:30 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -1,1 +1,40 @@
-# Add your code here
+songs = [
+  "Phoenix - 1901",
+  "Tokyo Police Club - Wait Up",
+  "Sufjan Stevens - Too Much",
+  "The Naked and the Famous - Young Blood",
+  "(Far From) Home - Tiga",
+  "The Cults - Abducted",
+  "Phoenix - Consolation Prizes",
+  "Harry Chapin - Cats in the Cradle",
+  "Amos Lee - Keep It Loose, Keep It Tight"
+]
+
+def run()
+  greeting
+  command = user_command
+end
+
+def greeting
+  puts "Hello! Please choose one of the following \n 'help',\n 'list',\n 'play',\n exit"
+end
+
+def user_command
+  gets.strip
+end
+
+def help
+
+end
+
+def play
+
+end
+
+def list
+
+end
+
+def exit_jukebox
+
+end
